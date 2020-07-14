ce92490c2750c92b09c24d0af61be5d6d24bbb20
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-14 16:50:49 +0700

    save string.py

---
+++ PrintString.py
@@ -0,0 +1,10 @@
+print('hello world')
+print('information')
+print('Thailand')
+print("I'm hungry")
+print('he ask me "who got the book?"')
+print("I'm good and \"I will do my best\"")
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
